0ab93c11b0a34b67abb7e65c607f2d00b9e63d69
Brandon Moss <88378432+brandonmoss-99@users.noreply.github.com>
2022-08-11 21:45:11 +0100

    Add moveCircleX

---
+++ moveCircleX/main.py
@@ -0,0 +1,46 @@
+from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY_2, PEN_P4
+
+# Use P4 colour mode, we don't need more than 16 colours. Saves on RAM
+display = PicoGraphics(display=DISPLAY_PICO_DISPLAY_2, pen_type=PEN_P4)
+display.set_backlight(0.5)
+w, h = display.get_bounds()
+
+# Store the centre of the display X,Y
+cWidth = w >> 1
+cHeight = h >> 1
+
+RED = display.create_pen(255,0,0)
+GREEN = display.create_pen(0,255,0)
+BLACK = display.create_pen(0,0,0)
+
+def clear():
+    display.set_pen(BLACK)
+    display.clear()
+    display.update()
+
+# Hidden black square following behind the circle being drawn, to clear up 
+# it's path, without having to perform a full display clear
+def clearPosXMovCircle(x, y, r):
+    display.set_pen(BLACK)
+    # Start point (X,Y), width and height of rectangle
+    display.rectangle(x-2*r, y-r, r*2+1, r*2+1)
+
+# Draw black square to clear up, instead of performing a full display clear
+def clearStillCircle(x, y, r):
+    display.set_pen(BLACK)
+    display.rectangle(x-r, y-r, r*2+1, r*2+1)
+
+clear()
+while True:
+    for radius in range(5, 100, 25):
+        for xPos in range(0, w+radius+1, 5):
+            # Clear up trail from previous circle
+            clearPosXMovCircle(xPos, cHeight, radius)
+            display.set_pen(RED)
+            display.circle(cWidth, cHeight, radius)
+            display.set_pen(GREEN)
+            display.circle(xPos, cHeight, radius >> 1)
+            # Push new contents to screen
+            display.update()
+        # Clear up red circle pre-size change
+        clearStillCircle(cWidth, cHeight, radius)
---
+++ moveCircleX/readme.md
@@ -0,0 +1,4 @@
+# Demo
+This demo shows a circle in the centre of the screen, with a smaller circle moving across the X axis, overlapping. Each circle progressively gets larger, until it reaches a maximum, where it starts over.
+
+This demo taught me some very basics of Picographics, how to move a 2D object on a single axis, along with how to clear it up without performing a full display clear.
