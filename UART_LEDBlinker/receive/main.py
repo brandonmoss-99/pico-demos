5d3792be13242ec5dad19452b10afbe25f55ec1c
Brandon Moss <88378432+brandonmoss-99@users.noreply.github.com>
2023-06-08 09:05:14 +0100

    Add UART_LEDBlinker

---
+++ UART_LEDBlinker/receive/main.py
@@ -0,0 +1,23 @@
+import machine
+import time
+
+# Set to Pin GP5, NOT physically Pin 5 on the Pico
+rxPin = machine.Pin(5, machine.Pin.OUT)
+# Set to Pin GP4, NOT physically Pin 4 on the Pico
+txPin = machine.Pin(4, machine.Pin.IN)
+
+# Set LED to be the LED on the Pico
+led = machine.Pin("LED", machine.Pin.OUT)
+
+# Create UART configuration
+uart = machine.UART(1, baudrate=9600, rx=rxPin, tx=txPin, bits=8, parity=None, stop=2)
+
+# Send a 't' character over UART. If there's data back, read it, and if it's
+# an 'm' character from the other end, toggle the LED, then sleep for 1 second
+while True:
+    uart.write('m')
+    if uart.any():
+        data = uart.read()
+        if data == b't':
+            led.toggle()
+    time.sleep(1)
